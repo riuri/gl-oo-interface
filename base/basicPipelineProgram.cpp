42337865896917296b61110ac44652dee49a419c
Rodrigo Castiel <rcrs2cin.ufpe.br>
2016-03-03 17:10:18 -0800

    Adding dependencies and base files that I did’t implement.

---
+++ base/basicPipelineProgram.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <cstring>
+#include "openGLHeader.h"
+#include "basicPipelineProgram.h"
+using namespace std;
+
+
+int BasicPipelineProgram::Init(const char * shaderBasePath) 
+{
+  // Try to build and set variable handles.
+  if (BuildShadersFromFiles(shaderBasePath, "basic.vertexShader.glsl", "basic.fragmentShader.glsl") != 0
+   && SetShaderVariableHandles() == 0)
+  {
+    cout << "Failed to build the pipeline program." << endl;
+    return 1;
+  }
+
+  cout << "Successfully built the pipeline program." << endl;
+  return 0;
+}
+
+void BasicPipelineProgram::SetViewMatrix(OpenGLMatrix& viewMatrix)
+{
+  // pass "v" to the pipeline program, as the view matrix
+  GLfloat v[16];
+  viewMatrix.GetMatrix(v);
+  GLboolean isRowMajor = GL_FALSE; 
+  glUniformMatrix4fv(this->h_viewMatrix, 1, isRowMajor, v);
+}
+
+void BasicPipelineProgram::SetModelMatrix(OpenGLMatrix& modelMatrix)
+{
+  // pass "m" to the pipeline program, as the model matrix
+  GLfloat m[16];
+  modelMatrix.GetMatrix(m);
+  GLboolean isRowMajor = GL_FALSE; 
+  glUniformMatrix4fv(this->h_modelMatrix, 1, isRowMajor, m);
+}
+
+void BasicPipelineProgram::SetProjectionMatrix(OpenGLMatrix& projectionMatrix)
+{
+  // pass "m" to the pipeline program, as the projection matrix
+  GLfloat p[16];
+  projectionMatrix.GetMatrix(p);
+  GLboolean isRowMajor = GL_FALSE; 
+  glUniformMatrix4fv(this->h_projectionMatrix, 1, isRowMajor, p);
+}
+
+int BasicPipelineProgram::SetShaderVariableHandles() 
+{
+  this->h_projectionMatrix = glGetUniformLocation(this->programHandle, "P");
+  this->h_modelMatrix      = glGetUniformLocation(this->programHandle, "M");
+  this->h_viewMatrix       = glGetUniformLocation(this->programHandle, "V");
+
+  if (this->h_projectionMatrix == GL_INVALID_OPERATION)
+  {
+    cout << "Projection matrix location in shader wasn't found. " << endl;
+    return 0;
+  }
+  else if (this->h_modelMatrix == GL_INVALID_OPERATION)
+  {
+    cout << "Model matrix location in shader wasn't found. " << endl;
+    return 0;
+  }
+  else if (this->h_viewMatrix == GL_INVALID_OPERATION)
+  {
+    cout << "View matrix location in shader wasn't found. " << endl;
+    return 0;
+  }
+  else
+  {
+    return 1;  
+  }
+}
+
---
+++ base/basicPipelineProgram.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "pipelineProgram.h"
+#include "openGLMatrix.h"
+
+class BasicPipelineProgram : public PipelineProgram
+{
+public:
+  int Init(const char * shaderBasePath);  // init the program, "shaderBasePath" is the path to the folder containing the shaders
+  
+  void SetViewMatrix(OpenGLMatrix& viewMatrix);
+  void SetModelMatrix(OpenGLMatrix& modelMatrix);
+  void SetProjectionMatrix(OpenGLMatrix& projectionMatrix);
+
+protected:
+  virtual int SetShaderVariableHandles();
+
+  GLint h_projectionMatrix; // handle to the P matrix in the shader
+  GLint h_modelMatrix;  // handle to M matrix in the shader
+  GLint h_viewMatrix;   // handle to V matrix in the shader
+
+  // note: we can use the general syntax: h_name is a handle to the shader variable "name"
+};
+
