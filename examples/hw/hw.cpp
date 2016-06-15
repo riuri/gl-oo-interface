5bd8422833fddea48cb037698e63de6c9128516a
Rodrigo Castiel <rcrs2@cin.ufpe.br>
2016-06-15 13:40:56 -0700

    Refactoring entire library with a more complex system of makefiles (for several different modules).
    Basically, I will change the entire library with a better structure to
    allow large code without much trouble. Part 1.

---
+++ examples/hw/hw.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <gloo/test.h>
+
+int main()
+{
+  std::cout << "Hello, gl-oo-interface!" << std::endl;
+  TestFunc(10);
+
+  return 0;
+}
---
+++ modules/gloo_obj/test.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+
+void TestFunc(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    std::cout << "Hi __ " << i+1 << std::endl;
+  }
+}
